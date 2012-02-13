2f45b93a999b67cabfc8baa4e681191b4113dd5b
Ozan Turgut <ozanturgut@gmail.com>
2012-02-12 21:21:40 -0800

    fixed config and template

--- settings.py
+++ settings.py
@@ -63,10 +63,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    SITE_ROOT + "templates",
+    SITE_ROOT + "/templates",
 )
 
 INSTALLED_APPS = (
